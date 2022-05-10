5e74f844fd3c1a7f50848142c0492a737e439291
GreatCorn <scpisolation@yahoo.com>
2022-05-10 20:47:44 +0300

    ATHERD 1.0.2

--- ATHERD/sw.js
+++ ATHERD/sw.js
@@ -5,45 +5,40 @@ this.addEventListener('install', function (event){
 	event.waitUntil(
 		caches.open(staticCacheName)
 		.then(cache => {
-			return cache.addAll(['./manifest.json', './', './res.gcr']);
+			return cache.addAll(['./', './res.gcr']);
 		})
 	);
 });
 
 this.addEventListener('fetch', function (event){
 	//I DON'T FUCKING KNOW HOW SERVICE WORKERS WORK
-	if (navigator.onLine){
-		event.waitUntil(caches.open(staticCacheName).then(function(cache){
-				cache.delete('./res.gcr').then(function(response){
-					cache.add('./res.gcr');
-				});
-				cache.delete('./').then(function(response){
-					cache.add('./');
-				});
-			})
-		);
-		return;
-	}
-	//rawFile.send(null);
+	console.log(event);
 	console.log("Fetch event for ", event.request.url);
-	event.respondWith(
-		caches.match(event.request)
-		.then(response => {
+	event.respondWith(caches.match(event.request).then(function(response){
 			if (response) {
-				console.log("Found ", event.request.url, " in cache.");
-				return response;
+				let fname = event.request.url.substring(event.request.url.lastIndexOf('/')+1);
+				if(((fname == 'res.gcr') || (fname == '')) && navigator.onLine){
+					console.log(event.request.url);
+					caches.open(staticCacheName).then(function(cache){
+						cache.delete(event.request.url).then(function(response){
+							cache.add(event.request.url);
+							console.log("Updated ", event.request.url);
+						});
+					});
+				}
+				else{
+					console.log("Found ", event.request.url, " in cache.");
+					return response;
+				}
 			}
 			console.log("Network request for ", event.request.url);
-			return fetch(event.request)
-			.then(response => {
-				return caches.open(staticCacheName).then(cache => {
+			return fetch(event.request).then(function(response){
+				return caches.open(staticCacheName).then(function(cache){
 					cache.put(event.request.url, response.clone());
 					return response;
 				});
 			});
 
-		}).catch(error => {
-			// TODO 6 - Respond with custom offline page
 		})
 	);
 });
